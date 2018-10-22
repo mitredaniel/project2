8b7e35c83344bf0d8d228c9a4a11ccb8bd0373b8
mitredaniel <daniel.mitre@outlook.com>
2018-10-22 09:09:01 -0400

    last update..

--- PMSystem/src/main/java/com/revature/controller/UnitController.java
+++ PMSystem/src/main/java/com/revature/controller/UnitController.java
@@ -5,27 +5,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.revature.entities.P2Unit;
 import com.revature.service.UnitServiceImp;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
-
+@CrossOrigin( allowedHeaders="*", origins="http://localhost:4200")
+@EnableWebMvc
 public class UnitController {
 	
 	@Autowired
 	UnitServiceImp unitservice;
-	
+	@CrossOrigin(allowedHeaders="*",origins = "http://localhost:4200")
 	@PostMapping(value="/unit")
 	public List getUnits() {
 		return unitservice.availableUnit();
 		
 	}
-	
-	@PostMapping(value="/unitoccupied")
+	@CrossOrigin(allowedHeaders="*",origins = "http://localhost:4200")
+	@GetMapping(value="/unitoccupied")
 	public List getUnitoccupied() {
 		return unitservice.occupiedUnit();
 		
